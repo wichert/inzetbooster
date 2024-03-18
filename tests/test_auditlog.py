5da3ea3ed03a587d42a6d521f8461c5edc5adbab
Wichert Akkerman <wichert@wiggy.net>
2024-03-18 22:13:43 +0100

    Commit sqlite transactions
    If we don't do this data is not persisted.

--- tests/test_auditlog.py
+++ tests/test_auditlog.py
@@ -3,6 +3,8 @@ from inzetbooster.auditlog import AuditLog
 
 def test_mail_log() -> None:
     auditor = AuditLog(path=":memory:")
-    assert not auditor.was_mail_send("bar-shift", "alice@example.com")
-    auditor.log_mail("bar-shift", "alice@example.com", "12345")
-    assert auditor.was_mail_send("bar-shift", "alice@example.com")
+    assert not auditor.was_mail_send(145, "bar-shift", "alice@example.com")
+    auditor.log_mail(145, "bar-shift", "alice@example.com", "msgid")
+    assert auditor.was_mail_send(145, "bar-shift", "alice@example.com")
+    assert not auditor.was_mail_send(146, "bar-shift", "alice@example.com")
+    assert not auditor.was_mail_send(145, "shift-cancelled", "alice@example.com")
