c7784420cdbcda1dd305ca1d717a8d18e8a9b8c1
Wichert Akkerman <wichert@wiggy.net>
2024-03-22 10:38:17 +0100

    Add more tests

--- tests/test_shifts.py
+++ tests/test_shifts.py
@@ -1,8 +1,9 @@
 import datetime
 from typing import Any
+from unittest.mock import Mock, ANY
 
 import pytest
-from inzetbooster.shifts import Shift, parse_csv
+from inzetbooster.shifts import Shift, parse_csv, send_shift_mails
 
 
 @pytest.mark.parametrize(
@@ -137,3 +138,117 @@ def test_parse_csv():
             comments="",
         ),
     ]
+
+
+def test_send_shift_mails_uncovered_shift() -> None:
+    auditlog = Mock()
+    mailer = Mock()
+    send_shift_mails(
+        auditlog,
+        mailer,
+        [
+            Shift(
+                id=2926209,
+                group_id=10736,
+                group_name="Bar",
+                date=datetime.date(2024, 1, 13),
+                start_time=datetime.time(16, 0),
+                end_time=datetime.time(18, 0),
+                user_id=None,
+                user_name=None,
+                user_email=None,
+                comments="",
+            ),
+        ],
+    )
+    auditlog.was_mail_send.assert_not_called()
+    mailer.send.assert_not_called()
+
+
+def test_send_shift_mails_new_shift() -> None:
+    auditlog = Mock()
+    mailer = Mock()
+    auditlog.was_mail_send.return_value = False
+    send_shift_mails(
+        auditlog,
+        mailer,
+        [
+            Shift(
+                id=2926209,
+                group_id=10736,
+                group_name="Bar",
+                date=datetime.date(2024, 1, 13),
+                start_time=datetime.time(16, 0),
+                end_time=datetime.time(18, 0),
+                user_id="PRS2921",
+                user_name="Alice Alice",
+                user_email="alice@example.com",
+                comments="Nieuwjaarsborrel",
+            ),
+        ],
+    )
+    auditlog.was_mail_send.assert_called_once_with(
+        2926209, "shift-10736.html", "alice@example.com"
+    )
+    mailer.send.assert_called_once_with(
+        to_addr="alice@example.com",
+        to_name="Alice Alice",
+        subject="Aanmelding dienst Bar",
+        html=ANY,
+    )
+
+
+def test_send_shift_mails_already_send() -> None:
+    auditlog = Mock()
+    mailer = Mock()
+    auditlog.was_mail_send.return_value = True
+    send_shift_mails(
+        auditlog,
+        mailer,
+        [
+            Shift(
+                id=2926209,
+                group_id=10736,
+                group_name="Bar",
+                date=datetime.date(2024, 1, 13),
+                start_time=datetime.time(16, 0),
+                end_time=datetime.time(18, 0),
+                user_id="PRS2921",
+                user_name="Alice Alice",
+                user_email="alice@example.com",
+                comments="Nieuwjaarsborrel",
+            ),
+        ],
+    )
+    auditlog.was_mail_send.assert_called_once_with(
+        2926209, "shift-10736.html", "alice@example.com"
+    )
+    mailer.send.assert_not_called()
+
+
+def test_send_shift_mails_missing_template() -> None:
+    auditlog = Mock()
+    mailer = Mock()
+    auditlog.was_mail_send.return_value = False
+    send_shift_mails(
+        auditlog,
+        mailer,
+        [
+            Shift(
+                id=2926209,
+                group_id=404,
+                group_name="Bar",
+                date=datetime.date(2024, 1, 13),
+                start_time=datetime.time(16, 0),
+                end_time=datetime.time(18, 0),
+                user_id="PRS2921",
+                user_name="Alice Alice",
+                user_email="alice@example.com",
+                comments="Nieuwjaarsborrel",
+            ),
+        ],
+    )
+    auditlog.was_mail_send.assert_called_once_with(
+        2926209, "shift-404.html", "alice@example.com"
+    )
+    mailer.send.assert_not_called()
