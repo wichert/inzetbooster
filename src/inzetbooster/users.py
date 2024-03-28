caec25276d570048efb8b168090fda33c16e0bc6
Wichert Akkerman <wichert@wiggy.net>
2024-03-28 14:25:35 +0100

    Complete manegeplan-import logic

--- src/inzetbooster/users.py
+++ src/inzetbooster/users.py
@@ -1,6 +1,13 @@
-from openpyxl import load_workbook
+import csv
+import datetime
+import io
 from dataclasses import dataclass
-from typing import Iterable
+from typing import BinaryIO, Iterable
+
+import structlog
+from openpyxl import load_workbook
+
+logger = structlog.stdlib.get_logger(__name__)
 
 
 @dataclass
@@ -10,12 +17,68 @@ class Person:
     preposition: str
     surname: str
     email: str
+    username: str | None = None
+    exempt: bool | None = None
+    activated_date: datetime.date | None = None
+    deactived_date: datetime.date | None = None
+    role: str | None = None
+    last_login: datetime.datetime | None = None
 
     def is_valid(self) -> bool:
         return bool(self.firstname and self.surname and self.email)
 
+    def is_manegeplan_user(self) -> bool:
+        return self.id.startswith("PRS")
 
-def read_manegeplan_export(fn: str) -> Iterable[Person]:
+    @classmethod
+    def from_record(cls, record: dict[str, str]):
+        logger.debug("parsing CSV record", data=record)
+        return Person(
+            id=record["Gebruiker_id"],
+            firstname=record["Voornaam"],
+            preposition=record["Tussen"],
+            surname=record["Achternaam"],
+            email=record["Email"],
+            username=record["Gebruikersnaam"] or None,
+            exempt=record["Vrijgesteld"] == "true",
+            activated_date=_parse_date(record["Actief_datum"]),
+            deactived_date=_parse_date(record["Inactief_datum"]),
+            role=record["Rol_en_rechten"],
+            last_login=datetime.datetime.strptime(
+                record["Laatste_login"], "%Y-%m-%d %H:%M"
+            )
+            if record["Laatste_login"]
+            else None,
+        )
+
+
+def _parse_date(value: str) -> datetime.date | None:
+    return datetime.datetime.strptime(value, "%d-%m-%Y").date() if value else None
+
+
+def parse_csv(f: str) -> Iterable[Person]:
+    reader = csv.DictReader(io.StringIO(f, newline=""), dialect=csv.unix_dialect)
+    return [Person.from_record(row) for row in reader]
+
+
+def create_csv(people: Iterable[Person]) -> str:
+    output = io.StringIO(newline="")
+    writer = csv.writer(output, dialect=csv.unix_dialect)
+    writer.writerow(["Gebruiker_id", "Voornaam", "Tussen", "Achternaam", "Email"])
+    for person in people:
+        writer.writerow(
+            [
+                person.id,
+                person.firstname,
+                person.preposition,
+                person.surname,
+                person.email,
+            ]
+        )
+    return output.getvalue()
+
+
+def read_manegeplan_export(fn: str | BinaryIO) -> Iterable[Person]:
     wb = load_workbook(fn)
     if (sheet := wb.active) is None:
         raise ValueError("workbook has no active sheet")
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
+import datetime
 from pathlib import Path
+
 import pytest
-from inzetbooster.users import Person, read_manegeplan_export
+from inzetbooster.users import Person, parse_csv, read_manegeplan_export
 
 
 @pytest.mark.parametrize(
@@ -69,5 +71,55 @@ def test_read_manegeplan_export():
             preposition="",
             surname="",
             email="madonna",
+        ),
+    ]
+
+
+def test_parse_csv():
+    input = """\
+"Gebruiker_id","Voornaam","Tussen","Achternaam","Email","Gebruikersnaam","Vrijgesteld","Actief_datum","Inactief_datum","Herinneringen","Rol_en_rechten","Opmerkingen","Laatste_login"
+"2","Vrijwilligerscoordinator","","Liethorp","vrijwilligers@example.com","vrijwilligers","false","12-01-2024","","true","admin","","2024-03-25 11:59"
+"PRS100","","","Acme Ltd","info@acme.example.com","acme","false","20-04-2023","","true","user","","2024-02-01 17:31"
+"PRS103","Bianca","","Smith","bianca@example.com","BiancaPRS103","false","20-04-2023","","true","user","",""
+"""
+    assert parse_csv(input) == [
+        Person(
+            id="2",
+            firstname="Vrijwilligerscoordinator",
+            preposition="",
+            surname="Liethorp",
+            email="vrijwilligers@example.com",
+            username="vrijwilligers",
+            exempt=False,
+            activated_date=datetime.date(2024, 1, 12),
+            deactived_date=None,
+            role="admin",
+            last_login=datetime.datetime(2024, 3, 25, 11, 59),
+        ),
+        Person(
+            id="PRS100",
+            firstname="",
+            preposition="",
+            surname="Acme Ltd",
+            email="info@acme.example.com",
+            username="acme",
+            exempt=False,
+            activated_date=datetime.date(2023, 4, 20),
+            deactived_date=None,
+            role="user",
+            last_login=datetime.datetime(2024, 2, 1, 17, 31),
+        ),
+        Person(
+            id="PRS103",
+            firstname="Bianca",
+            preposition="",
+            surname="Smith",
+            email="bianca@example.com",
+            username="BiancaPRS103",
+            exempt=False,
+            activated_date=datetime.date(2023, 4, 20),
+            deactived_date=None,
+            role="user",
+            last_login=None,
         ),
     ]
